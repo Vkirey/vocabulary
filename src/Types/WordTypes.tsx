700e006911318c60bd9d84b8e962a1c6c2cbe799
Vasiliy Kirey <36670646+Vkirey@users.noreply.github.com>
2022-05-12 21:02:47 +0200

    commented types a bit

--- src/Types/WordTypes.tsx
+++ src/Types/WordTypes.tsx
@@ -1,3 +1,6 @@
+/**
+ * Type of word. Used in whole application, actually extends ScoreType but with a difference in field optionality
+ */
 export type Word = {
   english: string;
   german: string;
@@ -5,12 +8,18 @@ export type Word = {
   userInput?: string;
 };
 
+/**
+ * Context type to store all data we need in word provider with possible actions to mutate it
+ */
 export type WordsContextType = {
   words: Array<Word>;
   addWord: (english: string, german: string) => void;
   deleteWordByIndex: (index: number) => void;
 };
 
+/**
+ * Type to store data regarding user's interaction in test
+ */
 export type ScoreType = {
   success: boolean;
   userInput: string;
